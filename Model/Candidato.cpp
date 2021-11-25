cc1b72df33b730882693dadd1b568d35789b4095
IsaacEscobar <47890746+IsaacEscobar@users.noreply.github.com>
2021-11-25 17:04:26 -0500

    Pequeños cambios al diagrama y agregados atributos de candidato

--- Model/Candidato.cpp
+++ Model/Candidato.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "Candidato.h"
+
+Candidato::Candidato() {
+
+}
+
+string Candidato::getNombre() {
+    return nombre;
+}
+
+void Candidato::setNombre(string nombre) {
+    this->nombre = nombre;
+}
+
+string Candidato::getEmail() {
+    return email;
+}
+
+void Candidato::setEmail(string email) {
+    this->email = email;
+}
+
+string Candidato::getlinkedIn() {
+    return linkedIn;
+}
+
+void Candidato::setLinkedIn(string linkedIn) {
+    this->linkedIn = linkedIn;
+}
+
+string Candidato::getGithub() {
+    return github;
+}
+
+void Candidato::setGithub(string github) {
+    this->github = github;
+}
+
+int Candidato::getPasaporte() {
+    return pasaporte;
+}
+
+void Candidato::setPasaporte(int pasaporte) {
+    this->pasaporte = pasaporte;
+}
+
+Cultura Candidato::getNacionalidad() {
+    return nacionalidad;
+}
+
+void Candidato::setNacionalidad(Cultura nacionalidad) {
+    this->nacionalidad = nacionalidad;
+}
+
--- Model/Candidato.h
+++ Model/Candidato.h
@@ -5,8 +5,37 @@
 #ifndef RECRUITMENT_IER_EDLF_CANDIDATO_H
 #define RECRUITMENT_IER_EDLF_CANDIDATO_H
 
+#include "Cultura.h"
+#include <string>
+
+using std::string;
 
 class Candidato {
+
+    private:
+        string nombre;
+        string email;
+        string linkedIn;
+        string github;
+        int pasaporte;
+        Cultura nacionalidad;
+
+    public:
+        Candidato();
+
+        string getNombre();
+        void setNombre(string nombre);
+        string getEmail();
+        void setEmail(string email);
+        string getlinkedIn();
+        void setLinkedIn(string linkedIn);
+        string getGithub();
+        void setGithub(string github);
+        int getPasaporte();
+        void setPasaporte(int pasaporte);
+        Cultura getNacionalidad();
+        void setNacionalidad(Cultura nacionalidad);
+
 
 };
 
